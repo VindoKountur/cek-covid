a355f4e01bfd8742fc40dfca4383be46a59eb2e7
Vindo Kountur <vindokountur@gmail.com>
2020-04-07 01:04:00 +0800

    :bug: remove eslint warn

--- src/components/SpesifikProvinsi.js
+++ src/components/SpesifikProvinsi.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect , useCallback} from 'react';
+import React, {useState} from 'react';
 import { Bar } from "react-chartjs-2";
 
 export const SpesifikProvinsi = ({ prov }) => {

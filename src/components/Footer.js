caac3c09e90fdd7efc5febbf79e9950a65a5de6d
Vindo Kountur <vindokountur@gmail.com>
2020-04-07 01:28:32 +0800

    :sparkle: add some text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,12 @@ export const Footer = () => {
       <div className="copyright-text">
         <p>&copy; 2020, Vindo K </p>
       </div>
+      <div className="provided">
+        Data by: <a href="https://kawalcorona.com" target="_blank" rel="noopener noreferrer">Kawalcorona.com</a>
+      </div>
       <div className="find-me" style={{cursor: 'pointer'}}>
-        <a href="https://linkedin.com/in/lifeindo-kountur" target="_blank" rel="noopener noreferrer">
-          <img src={LinkedInIcon} alt="My LinkedIn" title="Lifeindo Kountur" />
+        <a href="https://linkedin.com/in/lifeindo-kountur" target="_blank" rel="noopener noreferrer" className="make-center">
+          <img src={LinkedInIcon} alt="My LinkedIn" title="Lifeindo Kountur" /> My LinkedIn
         </a>
       </div>
     </footer>
